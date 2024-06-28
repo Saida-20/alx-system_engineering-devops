47d9fa918dff1829ee5c9984e33c3dff678ac910
root <abdullahisaida3@gmail.com>
2024-06-28 13:07:35 -0700

    task 0

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+#creating a file in /tmp using puppet
+
+file {'config management':
+  path    =>'/tmp/school',
+  content =>'I love Puppet',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+}
